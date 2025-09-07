ca6d7a7a256be93250403b98203e6d4bd980c803
ytjbalwikobra-lang <ytjbalwikobra@gmail.com>
2025-09-07 21:40:40 +0000

    fix(build): resolve AdminLayout import explicitly with .tsx to satisfy Vercel resolver

--- src/layouts/AdminLayoutWrapper.tsx
+++ src/layouts/AdminLayoutWrapper.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import AdminLayout from './AdminLayout';
+import AdminLayout from './AdminLayout.tsx';
 
 const AdminLayoutWrapper: React.FC = () => {
   const location = useLocation();
